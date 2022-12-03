1de919495d484b58facf68aed9dad255d1e7538b
aziztheiri <alatheiri123@gmail.com>
2022-12-03 14:19:42 +0100

    add admin

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -32,4 +32,27 @@ class PatientController extends AbstractController
         ]);
       
     }
+    #[Route('list', name: 'new_app_list')]
+    public function patient_list(ManagerRegistry $doctrine){
+        $repository = $doctrine->getRepository(Patient::class);
+        $patients = $repository->findAll();
+        return $this->render('PatientList.html.twig',array('patients'=>$patients));
+
+    }
+    #[Route('edit/{id}', name: 'patient_edit')]
+    public function edit(ManagerRegistry $doctrine,Request $request,Patient $patient){
+        $form=$this->createform(PatientType::class, $patient);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $entityManager = $doctrine->getManager();
+            $entityManager->flush();
+            return new Response('patient modifié !');
+        }
+        return $this->renderForm('patient/add.html.twig',[
+            'form'=>$form,
+            'controller_name'=>'Add Patient'
+            
+        ]);
+
+    }
 }
